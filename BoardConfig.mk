8cf2e9e1ac0df47e139a56e5d89b090e456cd33e
Ethan Chen <intervigil@gmail.com>
2018-11-28 20:44:43 -0800

    Mi439: Build disabled vbmeta image
    Change-Id: I40a6218f36ba2439855b14506bad726a49de774e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,11 @@ include device/xiaomi/mithorium-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/Mi439
 
+# Android Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := pine,olive,olivelite,olivewood,olives,mi439,Mi439
 
