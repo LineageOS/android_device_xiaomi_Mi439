490631751726fa8a82db8efcb4a5fd6278efb92c
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-11 00:33:07 +0530

    Mi439: Specify AVB recovery flags
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>
    Change-Id: I576883b03b9c4239f9010bbc200179958e6fe95c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,10 @@ DEVICE_PATH := device/xiaomi/Mi439
 # Android Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := pine,olive,olivelite,olivewood,olives,mi439,Mi439
