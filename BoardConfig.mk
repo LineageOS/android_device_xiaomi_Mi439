a6d344489da44010b39b6f9b753af369d959f375
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-05-21 09:48:29 +0800

    olives: biometrics: sepolicy: Import from sdm439-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,5 +25,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-03-01
 
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/biometrics/sepolicy
+
 # Inherit from the proprietary version
 include vendor/xiaomi/olives/BoardConfigVendor.mk
