48019bab76a27e1639800b679adff07522df094f
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-05-24 00:06:03 +0800

    olives: Define screen resolution

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 # Inherit from sdm439-common
 $(call inherit-product, device/xiaomi/sdm439-common/sdm439.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1572
+TARGET_SCREEN_WIDTH := 720
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
