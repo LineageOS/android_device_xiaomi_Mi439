2f79d52d834c3cd13b21a404a6fa6d8b77faeefa
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-05-24 08:10:21 +0800

    olives: Import audio and sensor configs

--- device.mk
+++ device.mk
@@ -11,6 +11,15 @@ $(call inherit-product, device/xiaomi/sdm439-common/sdm439.mk)
 TARGET_SCREEN_HEIGHT := 1572
 TARGET_SCREEN_WIDTH := 720
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_sdm439_pm8953_c3ik.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_sdm439_pm8953_c3ik.xml
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
