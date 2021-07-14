4ddbe28b63edcea86b41cf0afbb78050396048b1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-14 16:19:30 +0200

    Mi439: Use main fstab also for recovery
    Change-Id: Icc934bac31b05e4ef45404019a0d2849651bb035

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
