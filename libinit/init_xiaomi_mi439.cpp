a9a131794442fea5b43a29b3c8e7c2d4a6ab37ca
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-08 16:12:34 +0100

    mi439: libinit: Set ACDB props per device
    * Xiaomi decided to hardcode Forte in libacdbloader but they haven't killed get_files_from_properties from working
    * We can use that to load proper ACDBs per device
    * Note for whoever wanna use this: please first check if you can do this I36180f73f38c8c6b1f75cb6be62757f3a1000e25
    
    Change-Id: I9193a23fa7600f722abc4396ae5cfccf804d05cf

--- libinit/init_xiaomi_mi439.cpp
+++ libinit/init_xiaomi_mi439.cpp
@@ -5,11 +5,16 @@
  */
 
 #include <libinit_dalvik_heap.h>
+#include <libinit_utils.h>
 #include <libinit_variant.h>
 
 #include "vendor_init.h"
 
 #include <android-base/file.h>
+#include <filesystem>
+
+using std::filesystem::directory_iterator;
+using std::filesystem::filesystem_error;
 
 static const variant_info_t pine_info = {
     .brand = "Xiaomi",
@@ -27,14 +32,28 @@ static const variant_info_t olive_info = {
     .build_fingerprint = "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys",
 };
 
+static void set_acdb_path_props(std::string device)
+{
+    int i = 0;
+    try {
+        for (const auto& acdb : directory_iterator("/vendor/etc/acdbdata/" + device + "/"))
+            property_override(("persist.vendor.audio.calfile" + std::to_string(i++)).c_str(), acdb.path().c_str());
+    } catch (const filesystem_error&) {
+        // Ignore
+    }
+}
+
 static void determine_device()
 {
     std::string fdt_model;
     android::base::ReadFileToString("/sys/firmware/devicetree/base/model", &fdt_model, true);
-    if (fdt_model.find("PINE QRD") != fdt_model.npos)
+    if (fdt_model.find("PINE QRD") != fdt_model.npos) {
         set_variant_props(pine_info);
-    else if (fdt_model.find("Olive QRD") != fdt_model.npos)
+        set_acdb_path_props("pine");
+    } else if (fdt_model.find("Olive QRD") != fdt_model.npos) {
         set_variant_props(olive_info);
+        set_acdb_path_props("olive");
+    }
 }
 
 void vendor_load_properties() {
