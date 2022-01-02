41b711ff6eadca4c7fb689f969d283020fe06013
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-01-02 18:04:51 +0800

    mi439: Differentiate mixer paths and sensor config
    This would need a patch on audio HAL for mixer paths.

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_sdm439_pm8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_sdm439_pm8953.xml
+    $(LOCAL_PATH)/audio/olive_mixer_paths_sdm439_pm8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/olive_mixer_paths_sdm439_pm8953.xml
 
 # Consumer IR
 PRODUCT_PACKAGES += \
@@ -45,7 +45,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+    $(LOCAL_PATH)/configs/sensors/olive__def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/olive__def_qcomdev.conf
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/bin/sensors.qti.pine)
+            sed -i 's|sensor_def_|pine___def_|g' "${2}"
+            ;;
+        vendor/bin/sensors.qti.olive)
+            sed -i 's|sensor_def_|olive__def_|g' "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
