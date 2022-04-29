ab6ee2f8696dc5fd5c77c40cdf6003e248163017
me-cafebabe <me_cafebabe@hotmail.com>
2022-04-29 21:23:53 +0800

    Mi439: Import decommonized configurations and adapt for mi439

--- Android.mk
+++ Android.mk
@@ -9,4 +9,12 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),Mi439)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+WCNSS_MAC_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/wlan_mac.bin
+$(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "WCNSS MAC bin link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /mnt/vendor/persist/.$(notdir $@) $@
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_MAC_SYMLINK)
+
 endif
