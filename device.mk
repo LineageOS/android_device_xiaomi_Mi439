0ee2facce3e45d526c930adfbf0a403c6b6bc95b
me-cafebabe <me_cafebabe@hotmail.com>
2022-04-29 22:08:41 +0800

    Mi439: keylayout: Import from stock

--- device.mk
+++ device.mk
@@ -43,6 +43,11 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mi439 \
     android.hardware.biometrics.fingerprint@2.2
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.xiaomi.device.rc
