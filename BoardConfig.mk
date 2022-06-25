0a0ca22cc03d6d93713c9008cd971f69b3ad994e
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-25 19:54:34 +0800

    Mi439: Specify a non-existent dt directory to avoid using dt fstab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_mi439
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/7824900.sdhci
+BOARD_KERNEL_CMDLINE += androidboot.android_dt_dir=/non-existent androidboot.boot_devices=soc/7824900.sdhci
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := mi439-perf_defconfig
