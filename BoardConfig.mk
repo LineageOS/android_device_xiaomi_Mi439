32bbbd9c80128cedafdc2208889ab5fc4ec0a4fb
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-23 01:04:04 +0800

    Mi439: Reserve 200 MB on SSI partitions again
    As we have more space than Mi8937, why not

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,7 @@ $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := 5120))
 
 $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 83886080)) # 80 MB
+    $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 209715200)) # 200 MB
 $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_PARTITION_RESERVED_SIZE := 41943040)) # 40 MB
 
