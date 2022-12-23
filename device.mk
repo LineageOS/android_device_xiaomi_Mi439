701ade9420291658bdfb01608756198117f1a5d8
me-cafebabe <me_cafebabe@hotmail.com>
2022-12-23 19:22:18 +0800

    Mi439: Build Camera HAL

--- device.mk
+++ device.mk
@@ -46,6 +46,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/pine_mixer_paths_sdm439_pm8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/pine_mixer_paths_sdm439_pm8953.xml \
     $(LOCAL_PATH)/audio/olive_mixer_paths_sdm439_pm8953.xml:$(TARGET_COPY_OUT_VENDOR)/etc/olive_mixer_paths_sdm439_pm8953.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8937
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mi439 \
