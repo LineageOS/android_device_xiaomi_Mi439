79b7794112eb9c614b7c7e84b5b02a3887d359d7
me-cafebabe <me.cafebabe@gmail.com>
2023-05-11 03:30:57 +0800

    Mi439: libinit: Read device variant from /sys/xiaomi-sdm439-mach
    Change-Id: I43da9521a5ee2e1afd363c5101de74164911ce67

--- libinit/init_xiaomi_mi439.cpp
+++ libinit/init_xiaomi_mi439.cpp
@@ -27,14 +27,37 @@ static const variant_info_t olive_info = {
     .build_fingerprint = "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys",
 };
 
+static const variant_info_t olivelite_info = {
+    .brand = "Xiaomi",
+    .device = "olivelite",
+    .marketname = "",
+    .model = "Redmi 8A",
+    .build_fingerprint = "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys",
+};
+
+static const variant_info_t olivewood_info = {
+    .brand = "Xiaomi",
+    .device = "olivewood",
+    .marketname = "",
+    .model = "Redmi 8A Dual",
+    .build_fingerprint = "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys",
+};
+
 static void determine_device()
 {
-    std::string fdt_model;
+    std::string fdt_model, mach_codename;
     android::base::ReadFileToString("/sys/firmware/devicetree/base/model", &fdt_model, true);
     if (fdt_model.find("PINE QRD") != fdt_model.npos) {
         set_variant_props(pine_info);
     } else if (fdt_model.find("Olive QRD") != fdt_model.npos) {
-        set_variant_props(olive_info);
+        android::base::ReadFileToString("/sys/xiaomi-sdm439-mach/codename", &mach_codename, true);
+        mach_codename.pop_back();
+        if (mach_codename == "olivelite")
+            set_variant_props(olivelite_info);
+        else if (mach_codename == "olivewood")
+            set_variant_props(olivewood_info);
+        else
+            set_variant_props(olive_info);
     }
 }
 
