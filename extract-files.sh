959fd775ac110800c851db4ee3f180f599929b07
Alexander Martinz <amartinz@shiftphones.com>
2023-09-27 17:13:12 +0200

    Mi439: fingerprint: use libhidlbase-v32 for goodix
    Test: run cts-on-gsi -m CtsBiometricsTestCases
    Test: run cts-on-gsi -m CtsHardwareTestCases -t android.hardware.fingerprint.cts.FingerprintManagerTest
    Test: run vts -m VtsHalBiometricsFingerprintV2_1TargetTest
    Test: run vts -m VtsHalBiometricsFingerprintV2_2TargetTest
    Change-Id: Iaef8ede57250aa171f9863e7e250c604ef241334

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
