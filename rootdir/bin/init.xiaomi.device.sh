1f01d8766d5df37b8c1ce7175f0e43214e3bd364
Yumi Yukimura <me.cafebabe@gmail.com>
2024-02-16 06:15:16 +0800

    Mi439: Read device variant from /sys/xiaomi-sdm439-mach/codename
    * Goodbye, (old) custom kernels
    
    Change-Id: Ie901ad03a4a967f656faa4a219cee1ed03a05ead

--- rootdir/bin/init.xiaomi.device.sh
+++ rootdir/bin/init.xiaomi.device.sh
@@ -8,19 +8,39 @@ set_acdb_path_props() {
 	done
 }
 
-case "$(cat /sys/firmware/devicetree/base/model)" in
-	"PINE QRD")
+case "$(cat /sys/xiaomi-sdm439-mach/codename)" in
+	"pine")
 		setprop ro.vendor.xiaomi.device pine
+		setprop ro.vendor.xiaomi.series pine
 		# Audio
 		set_acdb_path_props pine
 		;;
-	"Olive QRD")
+	"olive")
 		setprop ro.vendor.xiaomi.device olive
+		setprop ro.vendor.xiaomi.series olive
 		# Audio
 		set_acdb_path_props olive
 		# Camera
 		setprop persist.vendor.camera.aec.sync 1
 		setprop persist.vendor.camera.awb.sync 2
+		setprop persist.vendor.camera.expose.aux 1
+		# Charger
+		setprop persist.vendor.ctm.disallowed true
+		;;
+	"olivelite")
+		setprop ro.vendor.xiaomi.device olivelite
+		setprop ro.vendor.xiaomi.series olive
+		# Audio
+		set_acdb_path_props olive
+		# Charger
+		setprop persist.vendor.ctm.disallowed true
+		;;
+	"olivewood")
+		setprop ro.vendor.xiaomi.device olivewood
+		setprop ro.vendor.xiaomi.series olive
+		# Audio
+		set_acdb_path_props olive
+		# Camera
 		setprop persist.vendor.camera.expose.aux 1
 		# Charger
 		setprop persist.vendor.ctm.disallowed true
