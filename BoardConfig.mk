27741a22cc908177a28f2ee7e312a81844afe71b
Michael Bestas <mkbestas@lineageos.org>
2025-07-19 10:55:59 +0300

    Mi439: Migrate QTI power HAL to soong_config_set
    Change-Id: Ib80c15ee523130011147d378f665592dae501ec2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,9 +91,6 @@ ifneq ($(WITH_GMS),true)
 BOARD_PRODUCTIMAGE_PARTITION_RESERVED_SIZE := 1287651328 # 1228 MB
 endif
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/sys/dev/xiaomi_sdm439_touchscreen/enable_dt2w"
-
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
--- device.mk
+++ device.mk
@@ -91,6 +91,9 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 $(call soong_config_set,livedisplay_sysfs,enable_se,true)
 
+# Power
+$(call soong_config_set,qtipower,tap_to_wake_node,/proc/sys/dev/xiaomi_sdm439_touchscreen/enable_dt2w)
+
 # Recovery
 PRODUCT_COPY_FILES += \
     vendor/xiaomi/Mi439/proprietary/vendor/bin/hvdcp_opti:$(TARGET_COPY_OUT_RECOVERY)/root/system/bin/hvdcp_opti
