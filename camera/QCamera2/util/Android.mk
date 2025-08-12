2a121273169c40dd26f9c298d467cdee4e74c554
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-13 00:59:09 +0800

    Mi439: camera: Correct `LOCAL_MODULE_CLASS` for prebuilt module
    * Run dos2unix while at it
    
    Change-Id: I5f85d81d1fb58bbb23fce332ff853e0489591637

--- camera/QCamera2/util/Android.mk
+++ camera/QCamera2/util/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:=$(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE:= cameraconfig.txt
-LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES := cameraconfig.txt
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/etc/camera
